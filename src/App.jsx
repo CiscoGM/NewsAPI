f0d498e51fcfd8da8aab8a19a63c290c203ef1aa
FRANCIS\franc <ciscoallen12@gmail.com>
2024-11-25 17:12:48 -0600

    initial

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import { useState } from 'react'
+import './App.css'
+import NavBar from './Components/NavBar'
+import NewsBoard from './Components/NewsBoard'
+
+function App() {
+
+const [category,setCategory]=useState("general")
+
+  return (
+    <>
+      <div>
+
+    <NavBar setCategory={setCategory}/>
+    <NewsBoard category={category}/>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
